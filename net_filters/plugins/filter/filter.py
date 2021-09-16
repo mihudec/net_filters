52dca7757efe62f5158d56313235fd0bbb975f53
Miroslav Hudec <mijujda@gmail.com>
2021-09-16 11:18:33 +0200

    Strip namespace from NetFilters

--- net_filters/plugins/filter/filter.py
+++ net_filters/plugins/filter/filter.py
@@ -1,6 +1,8 @@
 from net_templates.filters import NetFilters
+from net_templates.filters import namespace_decorator
+
 
 class FilterModule(object):
-    
+
     def filters(self):
-        return NetFilters().filters()
+        return {k.split('.')[-1]: v for k, v in NetFilters().filters().items()}
